d630afcdc1ffa243c66abcb52c630bce0d6780e5
Goal000330 <ericsmith.dev101@gmail.com>
2022-06-09 07:18:12 +0300

    creating service card

---
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -0,0 +1,24 @@
+import "./ServiceCard.css";
+export default function ServiceCard() {
+  // interface Props {
+  //   children: React.ReactNode;
+  //   color?: string;
+  //   disabled?: boolean;
+  //   className?: any;
+  //   onClick?: () => void;
+  // }
+
+  return (
+    <>
+      <div className="c-root">
+        <div className="c-imageContainer">
+          <img src="" />
+        </div>
+        <div className="c-descContainer">
+          〜150,000円｜本格ホームページ・サービスページ制作
+        </div>
+        <div className="c-cardFooterContainer">{/* </> */}</div>
+      </div>
+    </>
+  );
+}
