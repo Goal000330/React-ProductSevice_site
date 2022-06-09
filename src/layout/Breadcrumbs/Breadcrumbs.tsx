1b6bca8aa343c1f8dfde3da46d2f069c294e9f69
genie125 <geniebaby1024@gmail.com>
2022-06-09 13:31:57 +0900

    footer
    finished footer style

---
+++ src/layout/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import { BreadcrumbsStyles } from "./BreadcrumbsStyles";
+
+export default function Breadcrumbs() {
+  const classes = BreadcrumbsStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.homeLink}>HOME</div>
+      <div className={classes.link}>
+        <i className='far fa-angle-right'></i>
+        アカウント情報
+      </div>
+    </div>
+  );
+}
--- src/layout/LayoutStyle.tsx
+++ src/layout/LayoutStyle.tsx
@@ -4,13 +4,12 @@ import { createStyles, Theme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 export const LayoutStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
-      display: "flex",
-    },
+    root: {},
     container: {
       background: "#ebeff2",
       width: "calc(100% - 300px)",
       marginLeft: "300px",
+      minHeight: "calc(100vh - 163px)",
     },
     footer: {},
   })
